508b6dd98fb72a191b292c4e319aa157b3c429d2
adwithmukherjee <adwithmukherjee@gmail.com>
2021-08-31 12:15:51 -0400

    test call quality

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -41,7 +41,7 @@ const signInWithGoogle = async () => {
       .auth()
       .setPersistence(
         process.env.NODE_ENV === "production"
-          ? firebase.auth.Auth.Persistence.LOCAL
+          ? firebase.auth.Auth.Persistence.SESSION //CHANGE THIS BACK TO LOCAL AFTER TESTING
           : firebase.auth.Auth.Persistence.SESSION
       )
       .then(() => {
