7503ece8f0ba44319e16b0be82a08f0b20656b0f
adwithmukherjee <adwithmukherjee@gmail.com>
2021-09-02 13:25:24 -0400

    ssl redirect

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const express = require("express");
+import sslRedirect from "heroku-ssl-redirect";
 const admin = require("firebase-admin");
 const path = require("path");
 const serviceAccount = require("./secret/huddle-7dff8-firebase-adminsdk-vnj3n-05c2a68ad5.json");
@@ -6,6 +7,7 @@ var cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(sslRedirect());
 app.use(cors());
 
 admin.initializeApp({
