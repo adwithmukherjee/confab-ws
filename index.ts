0d096609b493e1b0ecb107833b33854288acaeab
Krishna Vudata <kvudata@gmail.com>
2021-09-09 22:20:46 -0700

    Fix /contact-us path

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-const express = require("express");
+import express from "express";
 import sslRedirect from "heroku-ssl-redirect";
-const admin = require("firebase-admin");
-const path = require("path");
+import admin from "firebase-admin";
+import path from "path";
 const serviceAccount = require("./secret/huddle-7dff8-firebase-adminsdk-vnj3n-05c2a68ad5.json");
-var cors = require("cors");
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -15,8 +18,8 @@ admin.initializeApp({
   databaseURL: "https://huddle-7dff8.firebaseio.com",
 });
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -27,8 +30,11 @@ const sockets = require("./sockets")(io);
 io.on("connection", sockets);
 // In dev mode just hide hide app.uss(... ) below
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
+app.get("/contact-us", (_req, res) => {
+  res.redirect("https://bigmesslabs.notion.site/Contact-Us-fe070dbeb780434eb2039858480af6c7");
+});
 app.get("*", (req: any, res: any) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
