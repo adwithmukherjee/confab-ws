516de08526a3cea6a2f491100b90f7da8b781a12
Champani Udayangani <wcuhamy@outlook.com>
2021-09-14 01:45:53 +0530

    callroom redesigning

--- client/src/components/QRCodeDisplay.tsx
+++ client/src/components/QRCodeDisplay.tsx
@@ -3,6 +3,7 @@ import { Paper } from "@material-ui/core";
 import QRCode from "qrcode.react";
 import { useStyles } from "./Call";
 
+
 interface QRCodeDisplayProps {
   channel: string;
 }
@@ -13,25 +14,15 @@ const QRCodeDisplay = (props: QRCodeDisplayProps) => {
     return <div />;
   } else {
     return (
-      <div style={{ position: "fixed", bottom: 0, right: 0 }}>
-        <div className={classes.qrCodeDisplayStyle}>
-          <Paper elevation={3} className={classes.qrCodeStyle}>
-            <QRCode
-              style={{ width: "100%", height: "100%" }}
-              value={`https://confab.bigmesslabs.com/call/${props.channel}`}
-            />
-          </Paper>
-          <div
-            style={{
-              width: 120,
-              textAlign: "center",
-              fontSize: "10px",
-              fontWeight: "bold",
-              color: "white",
-            }}
-          >
-            Scan to join this room on your phone!
-          </div>
+      <div className={classes.qrCodeDisplayStyle}>
+        <Paper elevation={3} className={classes.qrCodeStyle}>
+          <QRCode
+            style={{ width: "100%", height: "100%" }}
+            value={`https://confab.bigmesslabs.com/call/${props.channel}`}
+          />
+        </Paper>
+        <div className={classes.callroomQRInfotip}>
+          Scan to join this room on your phone!
         </div>
       </div>
     );
