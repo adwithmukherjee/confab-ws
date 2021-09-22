7d66c40f0e4304479c815fcc734f3fe11145739a
adwithmukherjee <adwithmukherjee@gmail.com>
2021-09-22 18:53:14 -0400

    test confg var

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.tsx";
-
+console.log("testing");
+console.log(process.env.FIREBASE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
     <App />
